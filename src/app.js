b7e008adf03fb66d4667764ebb23b121cd79f28c
shashanknv <shashanknv333@gmail.com>
2024-12-14 12:08:18 +0530

    Authentication, JWT & Cookies

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.post("/login", async (req, res) => {
 
       //Add the token to cookie and send the response back to the user
       res.cookie("token", token, {
-        expires: new Date(Date.mow() + 8 * 3600000),
+        expires: new Date(Date.now() + 8 * 3600000),
       });
       res.send("Login successful!!");
     } else {
