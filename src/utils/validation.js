672a40f5c8dfe386bae8c9d831991004c56cb7a9
shashanknv <shashanknv333@gmail.com>
2024-12-24 23:20:12 +0530

    edit profile API

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,8 +13,19 @@ const validateSignUpData = (req) => {
     else if(!validator.isStrongPassword(password)){
         throw new Error("Please enter a strong password!");
     }
-}
+};
+
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "photoUrl", "gender", "about", "skills",];
+
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+        allowedEditFields.includes(field)
+    );
+
+    return isEditAllowed;
+};
 
 module.exports = {
     validateSignUpData,
+    validateEditProfileData,
 }
